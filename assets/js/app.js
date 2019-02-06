e5fe567aceb6df12ef42fc638f3031def4e05daf
Michael DeFazio <mdefazio1@gmail.com>
2019-02-06 09:40:10 +0100

    v4.5
    Add in anime.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// const anime = require('vendor/anime.min.js');
 const accordionTitles = document.querySelectorAll('.accordion-title');
 
 
@@ -6,10 +7,19 @@ function showList(element) {
     // element.classList.toggle('show');
 }
 
+console.log("App.js loaded");
 
 accordionTitles.forEach(function (element) {
     element.addEventListener('click', function() {
-        console.log(this);
+        // console.log(this);
         this.classList.toggle('show');
     });
+});
+
+
+anime({
+  targets: ['.carousel-image', '.carousel-quote'],
+  duration: 1300,
+  opacity: [0, 1],
+  easing: 'easeOutCubic'
 });
